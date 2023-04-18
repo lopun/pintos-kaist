bc05a09c8d57a435d1c36b765925ad95748f7526
로펀 <lopun@lopeon-ui-MacBookPro.local>
2023-04-19 00:15:19 +0900

    bug fixing

--- threads/thread.c
+++ threads/thread.c
@@ -656,6 +656,19 @@ allocate_tid (void) {
 void thread_priority_donate(struct thread *target, int new_priority) {
     target->priority = new_priority;
 
+	// propagate priority donation to threads waiting on locks held by the target thread
+    struct list_elem *e;
+    for (e = list_begin (&target->locks); e != list_end (&target->locks); e = list_next (e)) {
+        struct lock *lock = list_entry (e, struct lock, elem);
+        if (lock->semaphore.value == 0 && lock->holder != NULL) { // lock is held and there are waiting threads
+            struct thread *holder = lock->holder;
+            if (holder->priority < new_priority) { // propagate donation
+                thread_priority_donate(holder, new_priority);
+            }
+        }
+    }
+
+
     if (target == thread_current() && !list_empty(&ready_list)) { // 현재 thread가 donate 받은 경우
         struct thread *next = list_entry(list_begin(&ready_list), struct thread, elem);
         if (next != NULL && next->priority > new_priority) { // ready_list의 가장 앞에 있는 thread의 priority가 더 높은 경우
